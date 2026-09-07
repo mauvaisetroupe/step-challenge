eebf8d56e10db8dcadce2ad2c47b3d32dedafb65
mauvaisetroupe <lionel.brol@gmail.com>
2026-09-07 11:58:09 +0200

    fix: tab icons

--- apps/mobile/src/app/(tabs)/_layout.tsx
+++ apps/mobile/src/app/(tabs)/_layout.tsx
@@ -4,13 +4,23 @@ export default function TabsLayout() {
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="home">
-        <NativeTabs.Trigger.Icon sf="house.fill" />
-        <NativeTabs.Trigger.Label>Accueil</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon
+          sf="house.fill"
+          md="home"
+        />
+        <NativeTabs.Trigger.Label>
+          Accueil
+        </NativeTabs.Trigger.Label>
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="stats">
-        <NativeTabs.Trigger.Icon sf="chart.bar.fill" />
-        <NativeTabs.Trigger.Label>Stats</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon
+          sf="chart.bar.fill"
+          md="bar_chart"
+        />
+        <NativeTabs.Trigger.Label>
+          Stats
+        </NativeTabs.Trigger.Label>
       </NativeTabs.Trigger>
     </NativeTabs>
   )
